ac46ed5598c1a9602b50c4c28daf45644611d4e7
joshuaiyumva <joshuaiyumva@gmail.com>
2020-10-02 20:26:05 +0200

    add changeable icons on the what to do section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document.ready(function(){
+// Design icon interface logic...
 $('p.design_icon').click(function () {
     $(this).hide();
     $('p.design_para').show();
@@ -7,4 +7,22 @@ $('p.design_icon').click(function () {
     $(this).hide();
     $('p.design_icon').show();
 });
+// Development icon interface logic...
+$('p.dev_icon').click(function () {
+    $(this).hide();
+    $('p.dev_para').show();
 });
+$('p.dev_para').click(function () {
+    $(this).hide();
+    $('p.dev_icon').show();
+});
+// Product icon interface logic...
+$('p.product_icon').click(function () {
+    $(this).hide();
+    $('p.product_para').show();
+});
+$('p.product_para').click(function () {
+    $(this).hide();
+    $('p.product_icon').show();
+});
+
